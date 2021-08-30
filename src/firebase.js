605a8af5b07cb17dce2e4ebeb11501eb72c53ecb
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-30 08:22:38 +0200

    config firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFkuS2OExM0HBOBz4XobwAKz57lpgiA8g",
+    authDomain: "clone-e4a13.firebaseapp.com",
+    projectId: "clone-e4a13",
+    storageBucket: "clone-e4a13.appspot.com",
+    messagingSenderId: "32329356357",
+    appId: "1:32329356357:web:4b4cddf54694db06dcbf2c",
+    measurementId: "G-KHT5Z5367K"
+  };
