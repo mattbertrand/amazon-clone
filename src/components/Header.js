e897cc69b24ec0ba306073436da0857b8ea58493
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-30 08:44:55 +0200

    first header component and styling

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../css/Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon-logo" />
+            <div className="header__search">
+                <input className="header__searchInput" type="text"/>
+            </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Matt</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
