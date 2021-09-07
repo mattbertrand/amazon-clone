b6d5cd95685996201ad3bb9ea5a140a21aef21b8
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-07 15:26:55 -0700

    Order component and styled

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import '../css/Orders.css';
 import { db } from '../firebase';
 import { useStateValue } from './StateProvider';
+import Order from './Order';
 
 function Orders() {
     const [{ basket, user}, dispatch] = useStateValue();
@@ -10,15 +11,16 @@ function Orders() {
 useEffect(() => {
     if(user) {
         db
-            .collection('users')
-            .doc(user?.uid)
-            .orderBy('created', 'desc')
-            .onSnapshot(snapshot => (
-                setOrders(snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    data: doc.data()
-                })))
-            ))
+        .collection('users')
+        .doc(user?.uid)
+        .collection('orders')
+        .orderBy('created', 'desc')
+        .onSnapshot(snapshot => (
+            setOrders(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        ))
     } else {
         setOrders([])
     }
