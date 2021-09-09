5d280f262758b1c3f7f5c4e9cf0dfced0fdaa849
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-08 17:53:21 -0700

    checkout when user

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -8,7 +8,7 @@ import { useHistory } from 'react-router-dom';
 
 function Subtotal() {
     const history = useHistory();
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
 
     return (
         <div className="subtotal">
@@ -29,8 +29,10 @@ function Subtotal() {
                 thousandSeparator={true}
                 prefix={"$"}
             />
-
-            <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
+				{user && (
+					<button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
+				)}
+            
         </div>
     )
 }
